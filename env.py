2059e9fe935b04a228b6eba8a9123e2815638f7d
BryonyRBlack <ikow8b1w@students.codeinstitute.net>
2025-04-15 15:39:32 +0100

    Add About app

--- env.py
+++ env.py
@@ -4,5 +4,5 @@ os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_v9IWrToK4HCh@ep-red-haze-a2316avy.eu-central-1.aws.neon.tech/card_drown_motor_551948")
 
 os.environ.setdefault(
-    "SECRET KEY", "3omRrAo3we"
+    "SECRET_KEY", "3omRrAo3we"
 )
